d22d74d8dabc25d3dcdb46701a83436df85062f2
Mario <icutum@hotmail.com>
2023-07-10 00:04:08 +0200

    Fix image to colored unicode with appended text

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import os
 import sys
-import json
 import requests
 from pathlib import Path
 
 # requirements
 import asyncio
 import aiohttp
+from rich import print
 from PIL import Image
-from colr import Colr
-# from termcolor import colored
 
 THIS_PATH = Path(__file__).parent.resolve()
+IMAGE_WIDTH = 40
 
 async def api_call(is_repo, name):
     BASE_URL = 'https://api.github.com/'
@@ -28,57 +26,20 @@ async def api_call(is_repo, name):
         content = await res.json()
         return content
 
-def image_to_ascii(url):
-    img_data = requests.get(url).content
-    file_name = url.split('/')[-1].split('?')[0]
-    user_img_location = Path(f'{THIS_PATH}/images/{file_name}.png')
-
-    with open(user_img_location, 'wb') as file:
-        file.write(img_data)
-
-    with Image.open(user_img_location) as image:
-        IMAGE_WIDTH = 25
-        MULTIPLIER = 0.45 # Used to fix the image height because the characters are taller
-
-        # Get initial width and height
-        width, height = image.size
-        aspect_ratio = height / width
-
-        # Fix the new height according to the aspect ratio (1:1)
-        IMAGE_HEIGHT = int(aspect_ratio * IMAGE_WIDTH * MULTIPLIER)
-
-        image = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
-        pixel_values = image.getdata()
-
-        ascii_text = ''
-        for index, character in enumerate(pixel_values):
-            if not isinstance(character, (tuple, list)):
-                continue
-
-            r, g, b = character
-
-            if (index != 0) and (index % IMAGE_WIDTH == 0):
-                ascii_text += '\n'
-
-            ascii_text += Colr().rgb(r, g, b, '\u2588')
-
-    user_img_location.unlink(missing_ok=True)
-
-    return ascii_text
-
 def fetch_repo(info):
     return {
         'owner': info['owner'],
-        # 'size': info['size'],
         'stars': info['stargazers_count'],
         'watchers': info['watchers_count'],
-        # 'issues': info['open_issues_count'],
         'forks': info['forks_count'],
         'archived': info['archived'],
         'disabled': info['disabled'],
         **({'license': info['license']['name']} if info['license'] is not None else {}),
         **({'forked_parent': info['parent']['owner']['html_url']} if info['fork'] else {}),
-        # TODO: Languages: like colors in neofetch, with colors and icons
+        # 'size': info['size'],
+        # 'issues': info['open_issues_count'],
+
+        # TODO: Languages: like colors in neofetch, with colors and icons, max 4 lines of height
     }
 
 def fetch_user(info):
@@ -117,23 +78,81 @@ def fetch_main(name):
     elif info['type'] == 'Organization':
         return generic_info
 
+def rgb_to_hex(r, g, b):
+    return f'#{r:02x}{g:02x}{b:02x}'
+
+def image_to_unicode(url):
+    UNICODE_BLOCK_CHAR = '\u2588'
+
+    file_name = url.split('/')[-1].split('?')[0]
+    user_img_location = Path(f'{THIS_PATH}/images/{file_name}.png')
+
+    img_data = requests.get(url).content
+
+    with open(user_img_location, 'wb') as file:
+        file.write(img_data)
+
+    with Image.open(user_img_location) as image:
+        MULTIPLIER = 0.45 # Used to fix the image height because the characters are taller
+
+        # Get initial width and height
+        width, height = image.size
+        aspect_ratio = height / width
+
+        # Fix the new height according to the aspect ratio (1:1)
+        IMAGE_HEIGHT = int(aspect_ratio * IMAGE_WIDTH * MULTIPLIER)
+
+        pixel_values = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT)).getdata()
+
+        unicode_text = ''
+        for index, character in enumerate(pixel_values):
+            # Retrieve all RGB values and discard alpha channel (rich doesn't support alpha channel)
+            r, g, b = character if len(character) == 3 else character[:-1]
+
+            # Adds new line when the index reaches the width limit
+            if (index != 0) and (index % IMAGE_WIDTH == 0):
+                unicode_text += '\n'
+
+            hex = rgb_to_hex(r, g, b)
+
+            unicode_text += f'[{hex}]{UNICODE_BLOCK_CHAR}[/{hex}]'
+
+        user_img_location.unlink(missing_ok=True)
+
+        return unicode_text
+
 def output_generator(fetched_info):
-    fetched_info['image'] = image_to_ascii(fetched_info['image'])
+    COLORED_CHAR_LENGTH = 20
 
-    # print(fetched_info)
-    print(fetched_info['image'])
-    print(fetched_info['followers'])
-    print(fetched_info['github_url'])
-    # print(json.decoder(fetched_info['image']))
+    fetched_info['image'] = image_to_unicode(fetched_info['image'])
 
-if __name__ == '__main__':
-    # Api call
+    formatted_output = ''
+    for index, char in enumerate(fetched_info['image']):
+        formatted_output += char
+
+        # Checks if the width limit is reached to append text
+        if index / COLORED_CHAR_LENGTH == IMAGE_WIDTH:
+            formatted_output = formatted_output.rstrip() + '\t\tprueba\n'
+
+    return formatted_output
+
+
+def main():
+    # API call
+    if len(sys.argv) == 1:
+        return print('One argument must be provided')
+
     name = sys.argv[1]
 
     fetched_info = fetch_main(name)
 
-    output_generator(fetched_info)
+    print(output_generator(fetched_info))
 
-    # print(image_to_ascii('https://avatars.githubusercontent.com/u/110683019?v=4'))
-    # print(image_to_ascii('https://avatars.githubusercontent.com/u/1024025?v=4'))
-    # print(image_to_ascii('https://avatars.githubusercontent.com/u/90156486?v=4'))
+    # In case rate limit is exceeded:
+    # https://avatars.githubusercontent.com/u/110683019?v=4
+    # https://avatars.githubusercontent.com/u/1024025?v=4
+    # https://avatars.githubusercontent.com/u/90156486?v=4
+
+
+if __name__ == '__main__':
+    main()
