8e9add6ba20b5e9dbb4475876428ca1b88e577dd
Mario <icutum@hotmail.com>
2023-07-09 18:17:05 +0200

    Done image to ASCII conversion

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from pathlib import Path
 import asyncio
 import aiohttp
 from PIL import Image
-from termcolor import colored
-from ascii_magic import AsciiArt
+from colr import Colr
+# from termcolor import colored
 
 THIS_PATH = Path(__file__).parent.resolve()
 
@@ -29,7 +29,6 @@ async def api_call(is_repo, name):
         return content
 
 def image_to_ascii(url):
-    ASCII_CHARS = ['B','S','#','&','@','$','%','*','!',':','.']
     img_data = requests.get(url).content
     file_name = url.split('/')[-1].split('?')[0]
     user_img_location = Path(f'{THIS_PATH}/images/{file_name}.png')
@@ -37,37 +36,35 @@ def image_to_ascii(url):
     with open(user_img_location, 'wb') as file:
         file.write(img_data)
 
-    my_art = AsciiArt.from_image(user_img_location)
-    my_art.to_terminal()
+    with Image.open(user_img_location) as image:
+        IMAGE_WIDTH = 50
+        MULTIPLIER = 0.45 # Used to fix the image height because the characters are taller
 
-    return my_art
-    # with Image.open(user_img_location) as image:
-    #     width, height = image.size
-    #     CORRECT_WIDTH = 0.5 # Fixes aspect ratio and ascii width issues because characters are always taller than wider
+        # Get initial width and height
+        width, height = image.size
+        aspect_ratio = height / width
 
-    #     aspect_ratio = height / width
-    #     new_width = 75
-    #     new_height = aspect_ratio * new_width * CORRECT_WIDTH
+        # Fix the new height according to the aspect ratio (1:1)
+        IMAGE_HEIGHT = int(aspect_ratio * IMAGE_WIDTH * MULTIPLIER)
 
-    #     pixels = [pixel for pixel in image.resize((new_width, int(new_height))).getdata()]
-    #     grayscale_pixels = image.resize((new_width, int(new_height))).convert('L').getdata()
+        image = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+        pixel_values = image.getdata()
 
+        ascii_text = ''
+        for index, character in enumerate(pixel_values):
+            if not isinstance(character, (tuple, list)):
+                continue
 
-    #     framed_pixels = ''.join([ASCII_CHARS[pixel//25] for pixel in grayscale_pixels])
+            r, g, b = character
 
-        # Gets all the pixels and creates a new array in which
-        # each position of the array is all the pixels needed
-        # to fill each column of the ascii art
+            if (index != 0) and (index % IMAGE_WIDTH == 0):
+                ascii_text += '\n'
 
-    # user_img_location.unlink(missing_ok=True)
+            ascii_text += Colr().rgb(r, g, b, '\u2588')
 
-    # colored_pixels = list(zip(framed_pixels, pixels))
-    # ascii_text = [colored_pixels[index:index + new_width] for index in range(0, len(framed_pixels), new_width)]
+    user_img_location.unlink(missing_ok=True)
 
-    # print(colored_pixels)
-    # print(ascii_text)
-
-    # return ascii_text
+    return ascii_text
 
 def fetch_repo(info):
     return {
@@ -114,7 +111,6 @@ def fetch_organization(info):
         'following': info['following'],
     }
 
-
 def fetch_main(name):
     is_repo = '/' in name
 
@@ -145,10 +141,6 @@ if __name__ == '__main__':
 
     # print(fetch_main(name))
 
-    image_to_ascii('https://avatars.githubusercontent.com/u/110683019?v=4')
-
-    # Image ascii convert
-
-    # for line in image_to_ascii('https://avatars.githubusercontent.com/u/110683019?v=4'):
-    #     for char in line:
-    #         print(colored(char[0], ))
+    print(image_to_ascii('https://avatars.githubusercontent.com/u/110683019?v=4'))
+    # print(image_to_ascii('https://avatars.githubusercontent.com/u/1024025?v=4'))
+    # print(image_to_ascii('https://avatars.githubusercontent.com/u/90156486?v=4'))
