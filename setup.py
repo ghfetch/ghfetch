8c75218c6655ab756d0bc39819a7457a48e7947d
Daniel Milenov <danimileniv@gmail.com>
2024-03-17 16:45:40 +0100

    ci: Update to 1.5.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='ghfetch-pip',
-      version='1.5.0',
+      version='1.5.3',
       description='CLI tool to fetch GitHub information',
       author='Nullgaro, Icutum',
       author_email='ghfetch.contact@gmail.com',
